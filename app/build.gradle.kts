725fc286257ae7a2c663939d333217b17c4c27cf
FelixMatrixar <qfelix0112@gmail.com>
2024-02-23 23:33:39 +0700

    change compileSDK from 33 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.chris.mastering_kotlin"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.chris.mastering_kotlin"
