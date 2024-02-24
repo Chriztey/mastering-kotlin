42b216f49d49836a7c008e980412d56565f5cc93
Chriztey <CHRISTIETEYDADDY@GMAIL.COM>
2024-02-24 14:49:13 +0700

    Update MainActivity.kt
    testing printOut float & double value

--- app/src/main/java/com/chris/mastering_kotlin/MainActivity.kt
+++ app/src/main/java/com/chris/mastering_kotlin/MainActivity.kt
@@ -12,21 +12,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.chris.mastering_kotlin.ui.theme.MasteringkotlinTheme
 
+
+// Testing printOut double & float value
+var circleRatio = 3.14
+var circleRatioFloat = 3.145678f //max float decimal digits are 7
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             MasteringkotlinTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    printTestDoubleFloat()
                 }
             }
+
         }
+
+
     }
+
 }
 
 @Composable
@@ -34,7 +45,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+
     )
+
+}
+
+fun printTestDoubleFloat(){
+    (println(circleRatio))
+    (println(circleRatioFloat))
 }
 
 @Preview(showBackground = true)
